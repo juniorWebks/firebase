61038c7ebcf6a8f8a4d8a0286e4ed932bd4a5e12
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-08-09 11:07:29 +0200

    datafirebase add

---
+++ firebase./src/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+ var config = {
+    apiKey: "AIzaSyCpkFehH5dFNaq0dBJzpSpGCmd2sjf36Qc",
+    authDomain: "ks-sandbox-2cc5e.firebaseapp.com",
+    databaseURL: "https://ks-sandbox-2cc5e.firebaseio.com",
+    projectId: "ks-sandbox-2cc5e",
+    storageBucket: "ks-sandbox-2cc5e.appspot.com",
+    messagingSenderId: "894593074412"
+  };
+  firebase.initializeApp(config);
+
+  const database = firebase.database()
+
+  export const database = firebase.database()
